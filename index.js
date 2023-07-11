f8b601a7d054c8dcc762f91e7a79a5119bad857b
Ji Chen <jaykchen@gmail.com>
2023-07-10 21:45:50 -0400

    wrapped as commandline tool

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+#!/usr/bin/env node
+
 const { GatewayIntentBits, Client } = require('discord.js');
 const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const config = require('./config.json');
 const path = require('path');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const dotenv = require('dotenv');
 
-const { clientId, guildId, token } = config;
+let clientId, guildId, token;
 
 const commands = [];
 
@@ -29,11 +33,11 @@ function loadCommandsFromDirectory(directory) {
     }
 }
 
-loadCommandsFromDirectory('commands');
+async function register() {
+    loadCommandsFromDirectory('commands');
 
-const rest = new REST().setToken(token);
+    const rest = new REST().setToken(token);
 
-(async () => {
     try {
         console.log('Started refreshing application (/) commands.');
 
@@ -46,30 +50,48 @@ const rest = new REST().setToken(token);
     } catch (error) {
         console.error(error);
     }
-})();
+}
 
+yargs(hideBin(process.argv))
+    .command('register', 'register commands', (yargs) => {
+        return yargs
+            .option('json', { describe: 'use config.json' })
+            .option('clientid', { describe: 'Client ID' })
+            .option('guildid', { describe: 'Guild ID' })
+            .option('token', { describe: 'Discord Token' })
+            .option('verbose', { describe: 'Print all the commands registered' });
+    }, async (argv) => {
+        if (argv.json) {
+            const config = require('./config.json');
+            ({ clientId, guildId, token } = config);
+        } else {
+            ({ clientid: clientId, guildid: guildId, token } = argv);
+        }
 
+      await register();
 
+      if (argv.verbose) {
+        const client = new Client({
+            intents: [GatewayIntentBits.Guilds]
+        });
 
+        client.once('ready', async () => {
+            let commands;
+            if (guildId) {
+                commands = await client.guilds.cache.get(guildId)?.commands.fetch();
+            } else {
+                commands = await client.application?.commands.fetch();
+            }
 
-const client = new Client({
-    intents: [GatewayIntentBits.Guilds]
-});
+          console.log('Commands:');
+          commands.forEach((command) => {
+              console.log(`Name: ${command.name}\nDescription: ${command.description}`);
+          });
+      });
 
-client.once('ready', async () => {
-    let commands;
-    if (guildId) {
-        commands = await client.guilds.cache.get(guildId)?.commands.fetch();
-    } else {
-        commands = await client.application?.commands.fetch();
-    }
-
-    console.log('Commands:');
-    commands.forEach((command) => {
-        console.log(`Name: ${command.name}\nDescription: ${command.description}`);
-    });
-});
-
-client.login(token);  // Enter your bot token here
-
-
+          client.login(token);
+      }
+  })
+    .demandCommand()
+    .help()
+    .argv;
